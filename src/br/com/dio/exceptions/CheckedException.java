6bd1bae4aec54942298bb70ae8dd1ed0adea53bf
joaojorge <joaojorgedosanjos@gmail.com>
2023-07-12 15:38:12 -0300

    Checked exceptions Parte 1 - código precisar resolver as exceptions

--- src/br/com/dio/exceptions/CheckedException.java
+++ src/br/com/dio/exceptions/CheckedException.java
@@ -1,7 +1,7 @@
 package br.com.dio.exceptions;
 
 import java.io.*;
-
+//
 public class CheckedException {
     public static void main(String[] args) {
         String nomeDoArquivo = "romances-blake-crouch.txt";
